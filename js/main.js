d054273c4687b530fd2863007f9d1c134a7f44c7
spade69 <417298586@qq.com>
2016-07-14 19:10:57 +0800

    Sidebar

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
+
+
+function createSideBar(ele){
+	//创建几个选项tap并用bootstrap附上样式
+	var $personal=$('<div class="thumbnail"><img src="../images/headPhoto.png" class="img-circle"></div>');
+	var $tap=$('<a href="#" class="list-group-item active"><span class="glyphicon glyphicon-tag"></span>Content</a>');
+	//var $tap1=
+	ele.append($personal);
+	ele.append($tap);
+}
+
 $(function(){
 	var $ali = $('#tab>li>a');
 	var $li = $('#tab>li');
 	var $ul = $('#tab');
-						
+	var $left=$('#left');
+
 		$ali.mouseover(function(){
+			//$(this) indicates $ali dom node
 			var $this = $(this);
 			var $t = $this.index();
 			$this.addClass('current');
@@ -13,5 +26,7 @@ $(function(){
 
 		$ali.mouseout(function(){
 		 	$ali.removeClass('current');
-		 })
+		 });
+
+	createSideBar($left);
 })
