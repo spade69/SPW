d03c828d4b38734a1e029eb1b8333f96799977e2
spade69 <417298586@qq.com>
2017-02-21 20:31:07 +0800

    update website

--- js/common/localStorageImg.js
+++ js/common/localStorageImg.js
@@ -2,57 +2,57 @@
 Author:Jason
 */
 define(function(){
-	var createStorage=function(imgPath,ele){
-		this.src=imgPath;//var src="xxx.jpg";
-		this.ele=ele;
-	}
+    var createStorage=function(imgPath,ele){
+        this.src=imgPath;//var src="xxx.jpg";
+        this.ele=ele;
+    }
 
-	createStorage.prototype={
-		set:function(key,imgType){
-			var img=document.createElement('img');
-			//img.setAttribute('crossOrigin','annonymous');
-			//当图片加载完成时触发回调函数
-			img.src=this.src;
-			img.addEventListener('load',function(){
-				var imgcanvas=document.createElement("canvas");
-				var imgContext=imgcanvas.getContext("2d");
-				imgcanvas.style.width="50px";//CANVAS大小
-				imgcanvas.style.height="50px";
-				//确保canvas元素的大小和图片尺寸一致
-				//渲染图片到canvas中 定位以及宽高
-				// context.drawImage(img,x,y,width,height);
-				imgContext.drawImage(this,0,0,300,110);
-				//用data url形式取出
-				var imgAsDataURL=imgcanvas.toDataURL("image/"+imgType);
+    createStorage.prototype={
+        set:function(key,imgType){
+            var img=document.createElement('img');
+            //img.setAttribute('crossOrigin','annonymous');
+            //当图片加载完成时触发回调函数
+            img.src=this.src;
+            img.addEventListener('load',function(){
+                var imgcanvas=document.createElement("canvas");
+                var imgContext=imgcanvas.getContext("2d");
+                imgcanvas.style.width="50px";//CANVAS大小
+                imgcanvas.style.height="50px";
+                //确保canvas元素的大小和图片尺寸一致
+                //渲染图片到canvas中 定位以及宽高
+                // context.drawImage(img,x,y,width,height);
+                imgContext.drawImage(this,0,0,300,110);
+                //用data url形式取出
+                var imgAsDataURL=imgcanvas.toDataURL("image/"+imgType);
 
-				//保存到本地存储中
-				try{
-					localStorage.setItem(key,imgAsDataURL);//存到localstorage~！
-				}catch(e){
-					console.log("Storage failed:"+e);
-				}
-			},false);
-			
-		},
+                //保存到本地存储中
+                try{
+                    localStorage.setItem(key,imgAsDataURL);//存到localstorage~！
+                }catch(e){
+                    console.log("Storage failed:"+e);
+                }
+            },false);
+            
+        },
 
-		get:function(key){//从本地缓存取图片并渲染
-			var flag=true;
-			while((localStorage.getItem(key)===null)&&flag);
-			window.setTimeout(function(){
-				flag=false;
-			},3000);
-			var srcStr=localStorage.getItem(key);
-			var imgObj=document.createElement('img');
-			imgObj.src=srcStr;
-			imgObj.style.width="50px"; //这个会截断图片
-			imgObj.style.height="60px";
-			imgObj.style.marginLeft="20px";
-			//document.body.appendChild(imgObj);
-			this.ele.appendChild(imgObj);
-		}
-	}
+        get:function(key){//从本地缓存取图片并渲染
+            var flag=true;
+            while((localStorage.getItem(key)===null)&&flag);
+            window.setTimeout(function(){
+                flag=false;
+            },3000);
+            var srcStr=localStorage.getItem(key);
+            var imgObj=document.createElement('img');
+            imgObj.src=srcStr;
+            imgObj.style.width="50px"; //这个会截断图片
+            imgObj.style.height="60px";
+            imgObj.style.marginLeft="20px";
+            //document.body.appendChild(imgObj);
+            this.ele.appendChild(imgObj);
+        }
+    }           
 
-	return{
-		createStorage:createStorage
-	}
+    return{
+        createStorage:createStorage
+    }
 });
--- js/common/verify.js
+++ js/common/verify.js
@@ -12,7 +12,7 @@
 1.字符数为4~16位
 2.每个英文字母、数字、英文符号长度为1
 3.每个汉字，中文符号长度为2
-	
+    
 Author:Jason
 Date:2016/06/06
 Modified:2016/09/02
@@ -26,206 +26,206 @@ Modified:2016/09/02
 define(['useful'],function(useful){
 //封装属性,这个构造函数意义不大,为什么封装的用不了？
 var vform=function(btn,items){
-	this.btn=btn;
-	this.check=new Array(false,false,false,false,false);
-	this.items=items;
-	
+    this.btn=btn;
+    this.check=new Array(false,false,false,false,false);
+    this.items=items;
+    
 };
 //原型
 
 vform.prototype={
-	constructor:vform, //构造函数
-	flag:0,
-	vName:function(info,name){
-		
-		var xnnn=name;//inputEles[0];
-		var arg=this.check;
+    constructor:vform, //构造函数
+    flag:0,
+    vName:function(info,name){
+        
+        var xnnn=name;//inputEles[0];
+        var arg=this.check;
 
-		function handle(arg){
-			var text=xnnn.value.trim();
-			var reaLen=text.glen();
-			arg[0]=false;
-			if(reaLen<1||text==""){
-				xnnn.style.borderColor="red";
-				info.innerHTML="<span style='color:red'>输入不能为空</span>";
+        function handle(arg){
+            var text=xnnn.value.trim();
+            var reaLen=text.glen();
+            arg[0]=false;
+            if(reaLen<1||text==""){
+                xnnn.style.borderColor="red";
+                info.innerHTML="<span style='color:red'>输入不能为空</span>";
 
-					//return;
-			}
-			else if(reaLen<4||reaLen>16){
-				xnnn.style.borderColor="red";
-				info.innerHTML="<span style='color:red'>请检查名称的</span>";
-			}
-			else{
-				arg[0]=true;
-				xnnn.style.borderColor="green";
-				info.innerHTML="<span style='color:green'>名称格式正确</span>";
-			}
+                    //return;
+            }
+            else if(reaLen<4||reaLen>16){
+                xnnn.style.borderColor="red";
+                info.innerHTML="<span style='color:red'>请检查名称的</span>";
+            }
+            else{
+                arg[0]=true;
+                xnnn.style.borderColor="green";
+                info.innerHTML="<span style='color:green'>名称格式正确</span>";
+            }
 
-		}//handle
+        }//handle
 
-		addEvent(xnnn,'blur',function(){handle(arg);});
-		addEvent(xnnn,'focus',function(){
-		info.innerHTML="<span style='color:gray'>必填，长度为4~16字符</span>";
-		});
-		
-	},
+        addEvent(xnnn,'blur',function(){handle(arg);});
+        addEvent(xnnn,'focus',function(){
+        info.innerHTML="<span style='color:gray'>必填，长度为4~16字符</span>";
+        });
+        
+    },
 
-	vPasswd:function(icode,code){
-		//var code=$("#passwd");
-		
-		var arg=this.check;
+    vPasswd:function(icode,code){
+        //var code=$("#passwd");
+        
+        var arg=this.check;
 
-		function handle(arg){
-			var text=code.value.trim();
-			arg[1]=false;
-			if(text.length<1||text===""){
-				code.style.borderColor="red";
-				icode.innerHTML="<span style='color:red'>输入不能为空</span>";
-			}   
-			else if(text.match(/^[a-zA-Z0-9]{6,16}$/))
-			{
-				arg[1]=true;
-				code.style.borderColor="green";
-				icode.innerHTML="<span style='color:green'>密码格式正确</span>";
-				flag=1;
-			}
-			else{
-				code.style.borderColor="red";
-				icode.innerHTML="<span style='color:red'>请输入6-16位字符且只能为数字和字母</span>";
-			}
-		}
+        function handle(arg){
+            var text=code.value.trim();
+            arg[1]=false;
+            if(text.length<1||text===""){
+                code.style.borderColor="red";
+                icode.innerHTML="<span style='color:red'>输入不能为空</span>";
+            }   
+            else if(text.match(/^[a-zA-Z0-9]{6,16}$/))
+            {
+                arg[1]=true;
+                code.style.borderColor="green";
+                icode.innerHTML="<span style='color:green'>密码格式正确</span>";
+                flag=1;
+            }
+            else{
+                code.style.borderColor="red";
+                icode.innerHTML="<span style='color:red'>请输入6-16位字符且只能为数字和字母</span>";
+            }
+        }
 
-		addEvent(code,'blur',function(){handle(arg);});
+        addEvent(code,'blur',function(){handle(arg);});
 
-		addEvent(code,'focus',function(){
-		icode.innerHTML="<span style='color:gray'>6~16位数字和字母</span>";
-		});
-	},
-	//fcode -- former(Original) password, xcode--xpassword
-	//dcodex --the info div under it
-	vPassx:function(dcodex,fcode,xcode){
-		// var xcode=inputEles[2];
-		// var fcode=inputEles[1];
-		var arg=this.check;
-		function handle(arg){
-			var text=xcode.value.trim();
-			var ftext=fcode.value.trim();
-			arg[2]=false;
-			if(text.length<1||text===""){
-				xcode.style.borderColor="red";
-				dcodex.innerHTML="<span style='color:red'>输入不能为空</span>";
-			}
-			else if(text===ftext&&flag===1){//&&flag===1
-				arg[2]=true;
-				xcode.style.borderColor="green";
-				dcodex.innerHTML="<span style='color:green'>密码正确</span>";
-			}
-			else{
-				xcode.style.borderColor="red";
-				dcodex.innerHTML="<span style='color:red'>两次输入的密码要相同</span>";
-			}
+        addEvent(code,'focus',function(){
+        icode.innerHTML="<span style='color:gray'>6~16位数字和字母</span>";
+        });
+    },
+    //fcode -- former(Original) password, xcode--xpassword
+    //dcodex --the info div under it
+    vPassx:function(dcodex,fcode,xcode){
+        // var xcode=inputEles[2];
+        // var fcode=inputEles[1];
+        var arg=this.check;
+        function handle(arg){
+            var text=xcode.value.trim();
+            var ftext=fcode.value.trim();
+            arg[2]=false;
+            if(text.length<1||text===""){
+                xcode.style.borderColor="red";
+                dcodex.innerHTML="<span style='color:red'>输入不能为空</span>";
+            }
+            else if(text===ftext&&flag===1){//&&flag===1
+                arg[2]=true;
+                xcode.style.borderColor="green";
+                dcodex.innerHTML="<span style='color:green'>密码正确</span>";
+            }
+            else{
+                xcode.style.borderColor="red";
+                dcodex.innerHTML="<span style='color:red'>两次输入的密码要相同</span>";
+            }
 
-		}
+        }
 
-		addEvent(xcode,'blur',function(){handle(arg);});
-		addEvent(xcode,'focus',function(){
-		dcodex.innerHTML="<span style='color:gray'>重复输入密码</span>";
-		});
-	},
+        addEvent(xcode,'blur',function(){handle(arg);});
+        addEvent(xcode,'focus',function(){
+        dcodex.innerHTML="<span style='color:gray'>重复输入密码</span>";
+        });
+    },
 
-	vmail:function(dmail,xmail){
-		//var xmail=inputEles[3];
-		var arg=this.check;
+    vmail:function(dmail,xmail){
+        //var xmail=inputEles[3];
+        var arg=this.check;
 
-			function handle(arg){
-			var str=xmail.value.trim();
-			var reg=new RegExp('^([a-zA-Z0-9_\.\-])+@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$','i');
-			arg[3]=false;
-			if(str.length<1||str===""){
-				xmail.style.borderColor="red";
-				dmail.innerHTML="<span style='color:red'>输入不能为空</span>";
-			}
-			else if(str.match(reg)){
-				arg[3]=true;
-				xmail.style.borderColor="green";	
-				dmail.innerHTML="<span style='color:green'>邮箱格式正确</span>";
-			}
-			else{
-				xmail.style.borderColor="red";
-				dmail.innerHTML="<span style='color:red'>邮箱格式错误</span>";
-			}
-		}
+            function handle(arg){
+            var str=xmail.value.trim();
+            var reg=new RegExp('^([a-zA-Z0-9_\.\-])+@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$','i');
+            arg[3]=false;
+            if(str.length<1||str===""){
+                xmail.style.borderColor="red";
+                dmail.innerHTML="<span style='color:red'>输入不能为空</span>";
+            }
+            else if(str.match(reg)){
+                arg[3]=true;
+                xmail.style.borderColor="green";    
+                dmail.innerHTML="<span style='color:green'>邮箱格式正确</span>";
+            }
+            else{
+                xmail.style.borderColor="red";
+                dmail.innerHTML="<span style='color:red'>邮箱格式错误</span>";
+            }
+        }
 
-			addEvent(xmail,'blur',function(){handle(arg);});
-			addEvent(xmail,'focus',function(){
-			dmail.innerHTML="<span style='color:gray'>exampleh@hah.com</span>";
-			});
-		
-	},
+            addEvent(xmail,'blur',function(){handle(arg);});
+            addEvent(xmail,'focus',function(){
+            dmail.innerHTML="<span style='color:gray'>exampleh@hah.com</span>";
+            });
+        
+    },
 
-	vphone:function(dphone,xphone){
-		//var xphone=inputEles[4];
-		var arg=this.check;
-			function handle(arg){
-			var str=xphone.value.trim();
-			arg[4]=false;
-			if(str.length<1||str===""){
-				xphone.style.borderColor="red";
-				dphone.innerHTML="<span style='color:red'>输入不能为空</span>";
-			}
-			else if(str.match(/0?(13|14|15|18)[0-9]{9}/)){
-				arg[4]=true;
-				xphone.style.borderColor="green";	
-				dphone.innerHTML="<span style='color:green'>电话格式正确</span>";
-			}
-			else{
-				xphone.style.borderColor="red";
-				dphone.innerHTML="<span style='color:red'>电话格式错误</span>";
-			}
-		}
+    vphone:function(dphone,xphone){
+        //var xphone=inputEles[4];
+        var arg=this.check;
+            function handle(arg){
+            var str=xphone.value.trim();
+            arg[4]=false;
+            if(str.length<1||str===""){
+                xphone.style.borderColor="red";
+                dphone.innerHTML="<span style='color:red'>输入不能为空</span>";
+            }
+            else if(str.match(/0?(13|14|15|18)[0-9]{9}/)){
+                arg[4]=true;
+                xphone.style.borderColor="green";   
+                dphone.innerHTML="<span style='color:green'>电话格式正确</span>";
+            }
+            else{
+                xphone.style.borderColor="red";
+                dphone.innerHTML="<span style='color:red'>电话格式错误</span>";
+            }
+        }
 
-			addEvent(xphone,'blur',function(){handle(arg);});
-			addEvent(xphone,'focus',function(){
-			dphone.innerHTML="<span style='color:gray'>请输入11位手机号码</span>";
-			});
-	},
+            addEvent(xphone,'blur',function(){handle(arg);});
+            addEvent(xphone,'focus',function(){
+            dphone.innerHTML="<span style='color:gray'>请输入11位手机号码</span>";
+            });
+    },
 
-	vbtn:function(xbtn){
-		var arg=this.check;
-		var count=this.items
-		//var xbtn=inputEles[5];
-		function handle(arg,count){
-			var flagx=1;
-			//5  it depends on how much you use
-			for(var i=0;i<count;i++){
-				if(arg[i]===false)
-				{
-					flagx=0;
-					break;
-				}
-			}
-			if(flagx)
-			{
-				//alert("格式正确,提交成功！");
-				xbtn.disabled=false;
-			}
-			else
-			{
-				
-				//alert("提交失败，请重新输入");
-				xbtn.disabled=true;
-			}
-		}
-		//addEvent(xbtn,'mouseover',function(){handle(arg);});
-		setInterval(function(){
-			handle(arg,count);
-		},1000);
-	}
+    vbtn:function(xbtn){
+        var arg=this.check;
+        var count=this.items
+        //var xbtn=inputEles[5];
+        function handle(arg,count){
+            var flagx=1;
+            //5  it depends on how much you use
+            for(var i=0;i<count;i++){
+                if(arg[i]===false)
+                {
+                    flagx=0;
+                    break;
+                }
+            }
+            if(flagx)
+            {
+                //alert("格式正确,提交成功！");
+                xbtn.disabled=false;
+            }
+            else
+            {
+                
+                //alert("提交失败，请重新输入");
+                xbtn.disabled=true;
+            }
+        }
+        //addEvent(xbtn,'mouseover',function(){handle(arg);});
+        setInterval(function(){
+            handle(arg,count);
+        },1000);
+    }
 
-	};//prototype
-	return{
-		vform:vform
-	}
+    };//prototype
+    return{
+        vform:vform
+    }
 });
 
 
